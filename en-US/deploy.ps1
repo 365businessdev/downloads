8b2b9cda952e264990b247334038a282aef225d4
Christoph Krieg <christoph.krieg@365businessdev.com>
2023-01-23 09:50:14 +0100

    Site updated at 01/23/2023 09:50 +01

--- en-US/deploy.ps1
+++ en-US/deploy.ps1
@@ -35,7 +35,7 @@ try {
     Set-Location ..\gh-pages
     git add .
     git commit -m $("Site updated at " + (Get-Date -UFormat "%m/%d/%Y %R %Z"))
-    git push origin main --force
+    git push origin gh-pages --force
     Set-Location ..\downloads
 } catch {
     Write-Host "[ERROR]" -ForegroundColor Red
